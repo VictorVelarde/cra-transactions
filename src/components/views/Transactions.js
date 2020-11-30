7e2091823f10f123d13253146d1081cda90c5a24
VictorVelarde <victor.velarde@gmail.com>
2020-11-30 19:57:57 +0100

    refactor: extract SQL into a external variable

--- src/components/views/Transactions.js
+++ src/components/views/Transactions.js
@@ -16,27 +16,29 @@ export default function Transactions() {
   const SOURCE_ID = `transactionsPerRegionSource`;
   const LAYER_ID = `transactionsPerRegion`;
 
+  const SQL_SOURCE = `
+    SELECT 
+      r.cartodb_id,
+      r.name, 
+      r.the_geom_webmercator,
+      SUM(t.amount) as sum,
+      AVG(t.amount) as avg,
+      COUNT(*) as count
+    FROM regions as r JOIN 
+      transactions as t
+      ON ST_Intersects(r.the_geom_webmercator, t.the_geom_webmercator)
+    GROUP BY 
+      r.cartodb_id,
+      r.name, 
+      r.the_geom_webmercator
+    `;
+
   useEffect(() => {
     // Add the source
     dispatch(
       addSource({
         id: SOURCE_ID,
-        data: `
-          SELECT 
-            r.cartodb_id,
-            r.name, 
-            r.the_geom_webmercator,
-            SUM(t.amount) as sum,
-            AVG(t.amount) as avg,
-            COUNT(*) as count
-          FROM regions as r JOIN 
-            transactions as t
-            ON ST_Intersects(r.the_geom_webmercator, t.the_geom_webmercator)
-          GROUP BY 
-            r.cartodb_id,
-            r.name, 
-            r.the_geom_webmercator
-          `,
+        data: SQL_SOURCE,
         type: 'sql',
       })
     );
@@ -54,7 +56,7 @@ export default function Transactions() {
       dispatch(removeLayer(LAYER_ID));
       dispatch(removeSource(SOURCE_ID));
     };
-  }, [dispatch, SOURCE_ID, LAYER_ID]);
+  }, [dispatch, SOURCE_ID, LAYER_ID, SQL_SOURCE]);
 
   return (
     <Grid container direction='row' className={classes.root}>
